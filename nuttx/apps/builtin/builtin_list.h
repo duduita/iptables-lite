adefad8e0c305c98849beb5f73cf66a4e496ef28
duduita <eduardomenezesm@msn.com>
2021-11-02 18:17:37 -0300

    wip

---
+++ nuttx/apps/builtin/builtin_list.h
@@ -0,0 +1,3 @@
+{ "sh", 100, 65536, sh_main },
+{ "hello", 100, 65536, hello_main },
+{ "nsh", 100, 65536, nsh_main },
---
+++ nuttx/apps/builtin/builtin_proto.h
@@ -0,0 +1,3 @@
+int sh_main(int argc, char *argv[]);
+int nsh_main(int argc, char *argv[]);
+int hello_main(int argc, char *argv[]);
